718aad131cf77056e05d8e1105a2e2f758e3d878
Ashirbad chand <chand.ash3@gmail.com>
2016-04-20 16:26:17 +0100

    Arrays

--- Day3/Arrrays3/Arrays3.js
+++ Day3/Arrrays3/Arrays3.js
@@ -5,14 +5,22 @@
 
       //function = set of instructions that can be called repeatedly in a program   
       function myfunction() {
-      var computer = new Array( document.getElementById("input1").value,  document.getElementById("input2").value,  document.getElementById("input3").value);
+      
+      // Arrays    
+      var fruits = new Array( document.getElementById("input1").value,  document.getElementById("input2").value, document.getElementById("input3").value);
           
-      document.getElementById("demo").innerHTML = computer[0] + " , " + computer[1] + " , " + computer[2];
+      document.getElementById("demo").innerHTML = fruits[0] + " , " + fruits[1] + " , " + fruits[2];
+     
+      //fruits.unshift("mangos", "grapes");
+      //document.getElementById("demo").innerHTML = fruits; 
+      fruits.shift("mangos", "grapes");
+      document.getElementById("demo").innerHTML = fruits;    
           
-      computer.push("disk");
-      document.getElementById("demo").innerHTML = computer + " " + computer.length;  
-    
-  
+      // push method adds new items on the end of array & return the length    
+      //computer.push("pears");
+      // document.getElementById("demo").innerHTML = fruits + " " + fruits.length;  
+      
+      
       
       }
     
